baa830546a66e8c21b86d83086572f5bda35568f
Talin Daga <talindaga692@gmail.com>
2026-02-23 21:18:07 +0530

    Add views and urls

---
+++ backend/analytics/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('datasets/', views.get_datasets, name='get_datasets'),
+    path('datasets/<int:id>/sample/', views.get_dataset_sample, name='get_dataset_sample'),
+]
--- backend/analytics/views.py
+++ backend/analytics/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .serializers import DatasetSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Dataset, Record
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+@api_view(['GET'])
+def get_datasets(request):
+    datasets = Dataset.objects.all()
+    serializer = DatasetSerializer(datasets, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_dataset_sample(request, id):
+
+    dataset = get_object_or_404(Dataset, id=id)
+    records = Record.objects.filter(dataset=dataset)[:5]
+    
+    sample_data = [record.row_data for record in records]
+    
+    return Response({
+        "dataset_id": dataset.id,
+        "dataset_name": dataset.name,
+        "sample": sample_data
+    })
